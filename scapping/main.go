60550b33b508c4278462eaae8bd834cb608cae58
Lukus <lukus.beaur@gmail.com>
2024-09-02 22:04:36 -0400

    Finished the logging, and proxy stuff. Going to run a speed test to see how fast I can make it.

--- scapping/main.go
+++ scapping/main.go
@@ -18,7 +18,7 @@ import (
 func main() {
 	starttime := time.Now()
 	var totalsize int
-
+	LogMessage("INFO", "Starting the scrapping process", nil)
 	csvfiles, err := scanCSVFiles("../links/")
 	if err != nil {
 		log.Fatalf("Error scaning for CSV files: %s", err)
@@ -33,7 +33,8 @@ func main() {
 		return
 	}
 	if records[0][0] == "end" {
-		fmt.Println("Starting fresh....")
+		//fmt.Println("Starting fresh....")
+		LogMessage("INFO", "Starting from beginning of list", nil)
 		for _, csvfilepath := range csvfiles {
 			file, reader := openFileReadAll(csvfilepath)
 			defer file.Close()
@@ -41,26 +42,29 @@ func main() {
 			if err != nil {
 				log.Fatalf("Unable to read file %s:%s", csvfilepath, err)
 			}
-			fmt.Printf("Working form %s", csvfilepath)
-
+			//fmt.Printf("Working form %s", csvfilepath)
 			for _, record := range records {
 				url := record[0]
 				//track current URL here
 				writeCurrentURL(url, "../last_URL.csv", []string{file.Name()})
-				fmt.Println("Processing URL:", url)
+				LogMessage("INFO", "Processing URL:"+url, nil)
+				//fmt.Println("Processing URL:", url)
 				totalsize += scrapeURL(url)
 			}
 		}
 	} else {
 		//find the index of record[0][0] The URL inside record[0][1] The file name and
 		// path: This will find the index in which the loop will start with------------->
-		fmt.Printf("Cont: %s on url %s\n", records[1][0], records[0][0])
+		//fmt.Printf("Cont: %s on url %s\n", records[1][0], records[0][0])
+		LogMessage("INFO", "Continue on:"+records[1][0]+" on url"+records[0][0], nil)
 		index, err := findLineIndex(records[1][0], records[0][0])
 		if err != nil {
-			log.Fatalf("There was an error with accessing the CSV %v", err)
+			LogMessage("ERROR", "There was an error with accessing the CSV", err)
+			//log.Fatalf("There was an error with accessing the CSV %v", err)
 			return
 		}
 		file, reader := openFileReadAll(records[1][0])
+		LogMessage("INFO", "Processing File:"+records[1][0], nil)
 		defer file.Close()
 		records, err := reader.ReadAll()
 		if err != nil {
@@ -72,12 +76,13 @@ func main() {
 			url := records[i][0]
 			//track current URL here
 			writeCurrentURL(url, "../last_URL.csv", []string{file.Name()})
-			fmt.Println("Processing URL:", url)
+			LogMessage("INFO", "Processing URL"+url, nil)
+			//fmt.Println("Processing URL:", url)
 			totalsize += scrapeURL(url)
 		}
 	}
 
-	fmt.Println("This if statement worked, it detected an 'end'")
+	//fmt.Println("This if statement worked, it detected an 'end'")
 
 	//Open tracker CSV file for URL. This will track where you are in the main list
 	//and if there is an interuption the loop will start on the last saved URL -------->
@@ -87,11 +92,13 @@ func main() {
 	//cont: searchfor index of that url in the master link file and start loop from there.
 	//Iterate through the records readall object. for each URL in the object scrape the
 	//table and save it to the 'Datatype_teamname.csv ---------------------------------->
-	fmt.Printf("The time is %s. Starting to pull data\n", starttime)
+	//fmt.Printf("The time is %s. Starting to pull data\n", starttime)
 	//TODO: create a graceful shutdown function. Channels, SIGS, and <- make.
 	elapsedTime := time.Since((starttime))
-	fmt.Printf("Total Elapsed time: %s\n", elapsedTime)
-	fmt.Printf("Total data collected: %d\n", totalsize)
+	LogMessage("INFO", "Total Elapsed Time: "+fmt.Sprint(elapsedTime), nil)
+	LogMessage("INFO", "Total File Size "+fmt.Sprint(totalsize), nil)
+	//fmt.Printf("Total Elapsed time: %s\n", elapsedTime)
+	//fmt.Printf("Total data collected: %d\n", totalsize)
 }
 func scrapeURL(url string) (totalsize int) {
 	var teamName, dataType, season string
@@ -112,9 +119,11 @@ func scrapeURL(url string) (totalsize int) {
 		season = "seasonDate_error"
 		dir := "../TeamData"
 		appendToFile(fmt.Sprintf("%s/url_Failure.csv", dir), []string{url})
-		fmt.Printf("Failed to extract data from URL: %s\n", url)
+		//fmt.Printf("Failed to extract data from URL: %s\n", url)
+		LogMessage("ERROR", "Failed to extract all info from URL"+url, nil)
 		return
 	}
+	LogMessage("INFO", "Extracted Data: "+dataType+"Extracted Team Name: "+teamName+"Extracted Season: "+season, nil)
 	//fmt.Println("Extracted data type:", dataType)
 	//fmt.Println("Extracted team name:", teamName)
 	//fmt.Println("Extracted season Date:", season)
@@ -130,6 +139,7 @@ func scrapeURL(url string) (totalsize int) {
 	defer writer.Flush()
 
 	//START: initiate a collector object
+	LogMessage("INFO", "Started new collector for fbref.com", nil)
 	c := colly.NewCollector(
 		colly.AllowedDomains("fbref.com"),
 		colly.ParseHTTPErrorResponse(),
@@ -142,24 +152,29 @@ func scrapeURL(url string) (totalsize int) {
 		log.Fatal(err)
 	}
 	c.SetProxyFunc(rp)
-
+	LogMessage("INFO", "Proxies have been set.", nil)
 	var startTime time.Time
 	var requestSize int
 	//lets try and connect first and print the call back of the request
 	//Start timer
 	c.OnRequest(func(r *colly.Request) {
 		startTime = time.Now()
-		fmt.Println("Visiting:", r.URL.String())
-		fmt.Printf("Proxy: %s\n", r.ProxyURL)
+		//fmt.Println("Visiting:", r.URL.String())
+		//fmt.Printf("Proxy: %s\n", r.ProxyURL)
+		LogMessage("INFO", "Visiting URL:"+r.URL.String(), nil)
+		LogMessage("INFO", "Proxy:"+r.ProxyURL, nil)
 	})
 	//on response lets check size of data
 	c.OnResponse(func(r *colly.Response) {
 		requestSize += len(r.Body)
-		fmt.Printf("Status code: %d\n", r.StatusCode)
+		//fmt.Printf("Status code: %d\n", r.StatusCode)
+		LogMessage("INFO", "Status Code:"+fmt.Sprint(r.StatusCode), nil)
 		//If there is a status code other than 200 (OK) then add the URL to the error
 		//list url_Failure.csv---------------------------------------------------------->
 		if r.StatusCode != 200 {
-			fmt.Printf("Status code mustnt be 200 right: %d\n", r.StatusCode)
+			LogMessage("CAUTION", "Status code is not 200", nil)
+			//fmt.Printf("Status code mustnt be 200 right: %d\n", r.StatusCode)
+			LogMessage("CAUTION", "URL failed, Writing it to url_failed.csv  URL: "+url, nil)
 			appendToFile("../url_Failure.csv", []string{url})
 			writer.Write([]string{url})
 			//} else {
@@ -168,8 +183,9 @@ func scrapeURL(url string) (totalsize int) {
 	})
 	c.OnScraped(func(r *colly.Response) {
 		elapsedTime := time.Since((startTime))
-		fmt.Printf("Elapsed Time for one URL: %s\n", elapsedTime)
-		fmt.Printf("Total Data pulled: %d bytes\n", requestSize)
+		LogMessage("INFO", "Scraped URL with "+fmt.Sprint(requestSize)+" in "+fmt.Sprint(elapsedTime), nil)
+		//fmt.Printf("Elapsed Time for one URL: %s\n", elapsedTime)
+		//fmt.Printf("Total Data pulled: %d bytes\n", requestSize)
 	})
 
 	//random delay rate limiter
@@ -211,7 +227,8 @@ func scrapeURL(url string) (totalsize int) {
 	//start and check for error
 	err = c.Visit(url)
 	if err != nil {
-		fmt.Println("Error visiting the site:", err)
+		LogMessage("ERROR", "Unable to visit site: "+url, err)
+		//fmt.Println("Error visiting the site:", err)
 	}
 	c.Wait()
 	return requestSize
@@ -219,6 +236,7 @@ func scrapeURL(url string) (totalsize int) {
 func createFile(filepath string) (*os.File, *csv.Writer) {
 	file, err := os.Create(filepath)
 	if err != nil {
+		LogMessage("ERROR", "Unable to create file", err)
 		log.Fatalf("Unable to create file %q: %s\n", filepath, err)
 	}
 	writer := csv.NewWriter(file)
@@ -233,10 +251,12 @@ func writeRecord(writer *csv.Writer, record []string) {
 func openFileReadAll(filepath string) (*os.File, *csv.Reader) {
 	file, err := os.Open(filepath)
 	if err != nil {
+		LogMessage("ERROR", "Unable to locate file", err)
 		log.Fatalf("Unable to open or locate file: %s\n", err)
 	}
 	reader := csv.NewReader(file)
 	if err != nil {
+		LogMessage("ERROR", "Unable to read from file", err)
 		log.Fatalf("Unable to read file: %s", err)
 	}
 	return file, reader
@@ -255,6 +275,7 @@ func appendFileAndWriter(filePath string) (*os.File, *csv.Writer) {
 	//This will open file and append lines instead of overwriting it ------------------>
 	file, err := os.OpenFile(filePath, os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
 	if err != nil {
+		LogMessage("ERROR", "Unable to open file during append", err)
 		log.Fatalf("Unable to open file %q: %s\n", filePath, err)
 	}
 	writer := csv.NewWriter(file)
@@ -271,6 +292,7 @@ func scanCSVFiles(dir string) ([]string, error) {
 	var csvFiles []string
 	err := filepath.Walk(dir, func(path string, info os.FileInfo, err error) error {
 		if err != nil {
+			LogMessage("ERROR", "Unable to scan CSV files", err)
 			return err
 		}
 		if !info.IsDir() && strings.HasSuffix(info.Name(), ".csv") {
@@ -285,6 +307,7 @@ func findLineIndex(filePath, value string) (int, error) {
 	defer file.Close()
 	records, err := reader.ReadAll()
 	if err != nil {
+		LogMessage("ERROR", "Unable to read CSV Data", err)
 		return -1, fmt.Errorf("unable to read CSV data: %v", err)
 	}
 	for i, record := range records {
@@ -295,5 +318,24 @@ func findLineIndex(filePath, value string) (int, error) {
 		}
 
 	}
+	LogMessage("ERROR", "Unable to find CSV", err)
 	return -1, fmt.Errorf("Unable to find CSV")
+}
+func LogMessage(severity string, action string, err error) {
+	//create, open, create writer, write record, defer close.
+	logfile, _ := appendFileAndWriter("../app.log")
+	defer logfile.Close()
+
+	log.SetOutput(logfile)
+	currentTime := time.Now().Format("2006-01-02 15:04:05")
+	logMessage := currentTime + " [" + severity + "] " + action
+
+	if err != nil {
+		logMessage += " - Error: " + err.Error()
+	}
+	// Write the log message to the file
+	_, errWrite := logfile.WriteString(logMessage + "\n")
+	if errWrite != nil {
+		log.Fatalf("Failed to write log message %s", err)
+	}
 }
